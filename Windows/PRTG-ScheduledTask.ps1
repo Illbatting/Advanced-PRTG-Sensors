94d0d8fc5b201aea266cf9311a972e0882eee746
Illbatting <carlen.roger@gmail.com>
2022-10-26 12:51:19 +0200

    Revert "Revert "Minor aesthetic changes""
    This reverts commit c8d3d83128ba6f6f2c57aa88406999009519f411.

--- Windows/PRTG-ScheduledTask.ps1
+++ Windows/PRTG-ScheduledTask.ps1
@@ -14,6 +14,11 @@
     LASTEDIT: 09/14/2016
     VERSION: 1.1
     KEYWORDS: PRTG, Windows, Schedule Task
+	
+	ChangeLog
+		2022-10-26, Roger Carlén -	Modified source-URL.
+									Function formatting.
+									
    
     .Link
     http://mycloudrevolution.com/
@@ -37,23 +42,32 @@ $Date = Get-Date -Format G
 
 #region: Functions
 ## Source: https://gallery.technet.microsoft.com/scriptcenter/Get-Scheduled-tasks-from-3a377294
-function Get-AllTaskSubFolders {
+function Get-AllTaskSubFolders
+{
     [cmdletbinding()]
     param (
-        # Set to use $Schedule as default parameter so it automatically list all files
-        # For current schedule object if it exists.
+        <#
+			Set to use $Schedule as default parameter so it automatically list all files
+			For current schedule object if it exists.
+		#>
         $FolderRef = $Schedule.getfolder("\")
     )
-    if ($FolderRef.Path -eq '\') {
+    if($FolderRef.Path -eq '\')
+	{
         $FolderRef
     }
-    if (-not $RootFolder) {
+    
+	if(-not $RootFolder)
+	{
         $ArrFolders = @()
-        if(($Folders = $folderRef.getfolders(1))) {
-            $Folders | ForEach-Object {
-                $ArrFolders += $_
-                if($_.getfolders(1)) {
-                    Get-AllTaskSubFolders -FolderRef $_
+        if(($Folders = $folderRef.getfolders(1)))
+		{
+            $Folders | ForEach-Object
+			{
+				$ArrFolders += $_
+                if($_.getfolders(1))
+				{
+					Get-AllTaskSubFolders -FolderRef $_
                 }
             }
         }
@@ -74,76 +88,86 @@ try {
 
 #region: Connect to Comuter and get Task Folders
 Write-Verbose "Connect to Comuter and get Task Folders..."
-$Schedule.connect($ComputerName) 
+$Schedule.connect($ComputerName)
 $AllFolders = Get-AllTaskSubFolders
 #endregion
 
 #region: Get Task Details
 Write-Verbose "Get Task Details..."
-[Array] $myTask = $AllFolders.GetTasks(1) | Where-Object {$_.name -eq $TaskName} | Foreach-Object {
-	        New-Object -TypeName PSCustomObject -Property @{
-	            'Name' = $_.name
-                'Path' = $_.path
-                'State' = switch ($_.State) {
-                    0 {'Unknown'}
-                    1 {'Disabled'}
-                    2 {'Queued'}
-                    3 {'Ready'}
-                    4 {'Running'}
-                    Default {'Unknown'}
-                }
-                'Enabled' = $_.enabled
-                'LastRunTime' = $_.lastruntime
-                'LastRunInHours' = [math]::round($(New-TimeSpan -Start $([datetime]$_.lastruntime) -End $Date).TotalHours,0)
-                'LastTaskResult' = $_.lasttaskresult
-                'NumberOfMissedRuns' = $_.numberofmissedruns
-                'NextRunTime' = $_.nextruntime
-                'ComputerName' = $Schedule.TargetServer
-            }
-    }
-if ($myTask.Length -gt 1) {
-        Write-Error "More than one Task fount. Exiting..."
-        Exit 1
+[Array] $myTask = $AllFolders.GetTasks(1) | Where-Object { $_.name -eq $TaskName } | Foreach-Object
+{
+		New-Object -TypeName PSCustomObject -Property @{
+			'Name' = $_.name
+			'Path' = $_.path
+			'State' = switch ($_.State)
+			{
+				0 { 'Unknown' }
+				1 { 'Disabled' }
+				2 { 'Queued' }
+				3 { 'Ready' }
+				4 { 'Running' }
+					Default {'Unknown' }
+			}
+			
+				'Enabled' = $_.enabled
+				'LastRunTime' = $_.lastruntime
+				'LastRunInHours' = [math]::round($(New-TimeSpan -Start $([datetime]$_.lastruntime) -End $Date).TotalHours,0)
+				'LastTaskResult' = $_.lasttaskresult
+				'NumberOfMissedRuns' = $_.numberofmissedruns
+				'NextRunTime' = $_.nextruntime
+				'ComputerName' = $Schedule.TargetServer
+			}
+}
+
+if($myTask.Length -gt 1)
+{
+	Write-Error "More than one Task fount. Exiting..."
+	Exit 1
 }
 #endregion
 
 #region: XML Output
 Write-Verbose "XML Output..."
-if ($myTask) {
-    Write-Host "<prtg>" 
-    foreach ($Object in $myTask){
-        $LastRunInHours = $Object.LastRunInHours
-        $LastTaskResult = $Object.LastTaskResult
-        $LastRunTime    = $Object.LastRunTime
-        $NextRunTime    = $Object.NextRunTime
-        Write-Host "<result>"
-                    "<channel>LastRunInHours</channel>"
-                    "<value>$LastRunInHours</value>"
-                    "<Unit>TimeHours</Unit>"
-                    "<showChart>1</showChart>"
-                    "<showTable>1</showTable>"
-                    "</result>" 
-        Write-Host "<result>"
-                    "<channel>LastTaskResult</channel>"
-                    "<value>$LastTaskResult</value>"
-                    "<DecimalMode>All</DecimalMode>"
-                    "<showChart>1</showChart>"
-                    "<showTable>1</showTable>"
-                    "<LimitMinError>0</LimitMinError>"
-                    "<LimitMaxError>0</LimitMaxError>"
-                    "<LimitMode>1</LimitMode>"
-                    "</result>" 
-            }
-    Write-Host "</prtg>" 
+if($myTask)
+{
+	Write-Host "<prtg>"
+    foreach($Object in $myTask)
+	{
+		$LastRunInHours = $Object.LastRunInHours
+		$LastTaskResult = $Object.LastTaskResult
+		$LastRunTime    = $Object.LastRunTime
+		$NextRunTime    = $Object.NextRunTime
+		
+		Write-Host "<result>"
+					"<channel>LastRunInHours</channel>"
+					"<value>$LastRunInHours</value>"
+					"<Unit>TimeHours</Unit>"
+					"<showChart>1</showChart>"
+					"<showTable>1</showTable>"
+					"</result>" 
+		Write-Host "<result>"
+					"<channel>LastTaskResult</channel>"
+					"<value>$LastTaskResult</value>"
+					"<DecimalMode>All</DecimalMode>"
+					"<showChart>1</showChart>"
+					"<showTable>1</showTable>"
+					"<LimitMinError>0</LimitMinError>"
+					"<LimitMaxError>0</LimitMaxError>"
+					"<LimitMode>1</LimitMode>"
+					"</result>" 
+	}
+    Write-Host "</prtg>"
 }
-else {
-    Write-Error "No Task with Name: $TaskName found. Exiting..."
-    Exit 1
+else
+{
+	Write-Error "No Task with Name: $TaskName found. Exiting..."
+    exit 1
 }
 #endregion
 
 #region: Debug
-if ($DebugPreference -eq "Inquire") {
-	$myTask | Select-Object ComputerName, Name, Path, Enabled, State, LastRunTime,  NextRunTime,  LastRunInHours, LastTaskResult, NumberOfMissedRuns | ft * -Autosize
+if($DebugPreference -eq "Inquire")
+{
+	$myTask | Select-Object ComputerName, Name, Path, Enabled, State, LastRunTime,  NextRunTime,  LastRunInHours, LastTaskResult, NumberOfMissedRuns #| ft * -Autosize
 }
 #endregion
